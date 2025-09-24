97fd2bdbf119a73bde0f3becfd623c5c41bfc8dd
EmptyOverlord <EmptyOverlord@gmail.com>
2025-09-24 09:16:40 +0500

    added coments

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,13 +6,11 @@ class AlienInvasio:
 
     def __init__(self):
         pygame.init()
+        self.colck = pygame.time.Clock()
 
         self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("👽Alien Invasion👽")
-        while True:
-             a=0
-             a += 1
-             pygame.display.set_caption(f"{a}👽Alien Invasion👽")
+        pygame.display.set_caption("👽Alien Invasion👽🚀")
+        self.bg_color = (230, 230, 230) # ! поменять на список чтобы можно было менять цвет фона прямо в игре или при событии
 
     
     def run_game(self):
@@ -20,10 +18,13 @@ class AlienInvasio:
         while True:
             # Оьслеживание событий клава-Мишь
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                if event.type == pygame.QUIT: 
                     sys.exit()
+            #при каждом проходе цыкла перерислвывается экран
+            self.screen.fill(self.bg_color)
             # отоброжение последнего прорисовоного экрана 
             pygame.display.flip()
+            self.colck.tick(60)
 
 if __name__ == '__main__':
     #создание экземпляра и запуск игры.
